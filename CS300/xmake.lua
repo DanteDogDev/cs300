67ebabc6cc2ea0dccb18a006387c1860b51e46b5
Dante Harper <dantedigdog@gmail.com>
2026-05-06 14:35:15 +0200

    init
    stuff
    
    init

---
+++ CS300/xmake.lua
@@ -0,0 +1,24 @@
+set_defaultmode("debug")
+
+add_rules("mode.debug", "mode.release")
+add_rules("plugin.compile_commands.autoupdate", { outputdir = "." })
+
+add_requires("libsdl3", "glew", "glm")
+
+set_languages("c++17")
+
+target("cs300", function()
+    set_kind("binary")
+    
+    add_packages("libsdl3", "glew", "glm")
+    
+    add_files("src/**.cpp")
+    add_includedirs("src")
+    set_rundir("$(projectdir)")
+
+    -- if is_plat("windows") then
+    --     add_syslinks("opengl32")
+    -- elseif is_plat("linux") then
+    --     add_syslinks("GL")
+    -- end
+end)
